5b600b5ff79e35392e5272a6a3c702b6acabee8f
CYBER DEXTER <152212325+darkewing@users.noreply.github.com>
2024-11-15 19:17:04 +0530

    Update Auto reply.js

--- Auto reply.js
+++ Auto reply.js
@@ -22,5 +22,41 @@
   {
     "triggerMessages": ["ඔබට සුභ රාත්‍රියක්", "සුභ රාත්‍රී", "Good Night", "ගුඩ් නයිට්", "Good night"],
     "audioUrl": "https://github.com/rcd-git-hub-official/status-send-raw-list/raw/refs/heads/main/sri-lanka-good-night.mp3"
+  },
+  {
+    "triggerMessages": ["හුත්ති", "ගෑනී", "Gani", "Hutti"],
+    "audioUrl": "https://github.com/DEXTER-ID-PROJECT/VOICE-/raw/refs/heads/main/Voice-clip/%E0%B6%B8%E0%B6%BD%E0%B7%94%E0%B6%9A%E0%B6%BB%E0%B6%BA%E0%B7%8F.mp3"
+  },
+  {
+    "triggerMessages": ["I love You", "Love you", "ආදරෙයි", "adarei"],
+    "audioUrl": "https://github.com/DEXTER-ID-PROJECT/VOICE-/raw/refs/heads/main/Voice-clip/comeback.mp3"
+  },
+   {
+    "triggerMessages": ["හුම්ම්", "hmmm", "Humm", "හුම්කො"],
+    "audioUrl": "https://github.com/DEXTER-ID-PROJECT/VOICE-/raw/refs/heads/main/Voice-clip/hmm.mp3"
+  },
+   {
+    "triggerMessages": ["Mk", "මොකද කරන්නෙ", "mokada karanne", "Mokada kalanne"],
+    "audioUrl": "https://github.com/DEXTER-ID-PROJECT/VOICE-/raw/refs/heads/main/Voice-clip/comeback.mp3"
+  },
+   {
+    "triggerMessages": ["Thank You", "තැන්ක් යූ", "thx", "thamks"],
+    "audioUrl": "https://github.com/DEXTER-ID-PROJECT/VOICE-/raw/refs/heads/main/Voice-clip/thanks.mp3"
+  },
+   {
+    "triggerMessages": ["xxx", "sexy", "හුකහන්", "xvideo", "sex"],
+    "audioUrl": "https://github.com/DEXTER-ID-PROJECT/VOICE-/raw/refs/heads/main/Voice-clip/xxx.mp3"
+  },
+   {
+    "triggerMessages": ["Ponnaya", "ගෝතයා", "පොන්නයා", "ponnaya"],
+    "audioUrl": "https://github.com/DEXTER-ID-PROJECT/VOICE-/raw/refs/heads/main/Voice-clip/%E0%B6%B4%E0%B7%9C%E0%B6%B1%E0%B7%8A%E0%B6%B1%E0%B6%BA%E0%B7%8F.mp3"
+  },
+   {
+    "triggerMessages": ["හුකනෝ", "හුකහන්", "hukahan", "Hukapam", "හුකාපන්"],
+    "audioUrl": ["https://github.com/DEXTER-ID-PROJECT/VOICE-/raw/refs/heads/main/Voice-clip/%E0%B7%84%E0%B7%94%E0%B6%9A%E0%B6%B1%E0%B7%9C%20.mp3"] 
+  },
+   {
+    "triggerMessages": ["Hutti", "හුත්ති", "වේසී", "wesi"],
+    "audioUrl": "https://github.com/DEXTER-ID-PROJECT/VOICE-/raw/refs/heads/main/Voice-clip/%E0%B7%84%E0%B7%94%E0%B6%AD%E0%B7%8A%E0%B6%AD%E0%B7%92%20.mp3"
   }
 ]
