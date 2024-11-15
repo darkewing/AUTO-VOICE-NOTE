a7445e3a3d7c653827a43dbaa1381e441cac02ed
CYBER DEXTER <152212325+darkewing@users.noreply.github.com>
2024-11-15 18:49:19 +0530

    Update Auto reply.js

--- Auto reply.js
+++ Auto reply.js
@@ -1,1 +1,26 @@
-
+[
+  {
+    "triggerMessages": ["hi", "hello", "hey", "greetings"],
+    "audioUrl": "https://github.com/rcd-git-hub-official/status-send-raw-list/raw/refs/heads/main/welcome.mp3"
+  },
+  {
+    "triggerMessages": ["good morning", "gm", "ගුඩ් මොනිම්", "awesome"],
+    "audioUrl": "https://github.com/DEXTER-ID-PROJECT/VOICE-/raw/refs/heads/main/Voice-clip/good%20morning%20.mp3"
+  },
+  {
+    "triggerMessages": ["bye", "goodbye", "good bye", "take care"],
+    "audioUrl": "https://github.com/DEXTER-ID-PROJECT/VOICE-/raw/refs/heads/main/Voice-clip/bye.mp3"
+  },
+  {
+    "triggerMessages": ["ආයුබෝවන්", "හෙලෝ", "ඔයාලට හෙලෝ", "ඔයාලට ගමන සතුටු"],
+    "audioUrl": "https://github.com/rcd-git-hub-official/status-send-raw-list/raw/refs/heads/main/sri-lanka-hello.mp3"
+  },
+  {
+    "triggerMessages": ["ඔබට සුභ දවසක්", "සුභ දවසක්", "සුබෝද", "සෙරී", "සුභ පැතුම්"],
+    "audioUrl": "https://github.com/rcd-git-hub-official/status-send-raw-list/raw/refs/heads/main/sri-lanka-good-morning.mp3"
+  },
+  {
+    "triggerMessages": ["ඔබට සුභ රෑ", "සුභ රාත්‍රී", "ඔබට ආයුබෝවන්", "කොහොමද"],
+    "audioUrl": "https://github.com/rcd-git-hub-official/status-send-raw-list/raw/refs/heads/main/sri-lanka-good-night.mp3"
+  }
+]
